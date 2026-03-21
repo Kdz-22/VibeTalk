62947038a41bcd8995894f545c46a7e48eadcb7d
Fernanda <143466223+Sunref@users.noreply.github.com>
2026-03-21 14:16:22 -0300

    NEW: Classe 'Message' pra lidar com as mensagens

---
+++ VibeTalk/app/src/main/java/com/example/vibetalk/Message.java
@@ -0,0 +1,19 @@
+package com.example.vibetalk;
+
+public class Message{
+
+	private String text;
+	private String time;
+	private boolean isSent = true // true - enviada por mim; false - recebida
+
+	public Message(String text, String time, boolean isSent){
+		this.text = text;
+		this.time = time;
+		this.isSent = isSent;
+	}
+
+	public String getText(){ return text; }
+	public String getTime(){ return time; }
+	public String getSent(){ return isSent; }
+
+}
